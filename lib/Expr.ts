de27525328e441a6bce78410978986ced1a90248
Benjamin Petermann <benjamin.petermann@gmx.at>
2024-03-20 22:40:39 +0100

    Refactor expression classes

--- lib/Expr.ts
+++ lib/Expr.ts
@@ -1,90 +1,127 @@
+import { Token } from './Token';
+
 abstract class Expr {
-  abstract accept(visitor: Visitor): void;
+  abstract accept<T>(visitor: Visitor<T>): T;
 }
 
-interface Visitor {
-  visitAssignExpr(expr: Assign): void;
-  visitBinaryExpr(expr: Binary): void;
-  visitCallExpr(expr: Call): void;
-  visitGetExpr(expr: Get): void;
-  visitGroupingExpr(expr: Grouping): void;
-  visitLiteralExpr(expr: Literal): void;
-  visitLogicalExpr(expr: Logical): void;
-  visitSetExpr(expr: SetExpr): void;
-  visitSuperExpr(expr: Super): void;
-  visitThisExpr(expr: This): void;
-  visitUnaryExpr(expr: Unary): void;
-  visitVariableExpr(expr: Variable): void;
+interface Visitor<T> {
+  visitAssignExpr(expr: Assign): T;
+  visitBinaryExpr(expr: Binary): T;
+  visitCallExpr(expr: Call): T;
+  visitGetExpr(expr: Get): T;
+  visitGroupingExpr(expr: Grouping): T;
+  visitLiteralExpr(expr: Literal): T;
+  visitLogicalExpr(expr: Logical): T;
+  visitSetExpr(expr: SetExpr): T;
+  visitSuperExpr(expr: Super): T;
+  visitThisExpr(expr: This): T;
+  visitUnaryExpr(expr: Unary): T;
+  visitVariableExpr(expr: Variable): T;
 }
 
 class Assign extends Expr {
-  override accept(visitor: Visitor): void {
-    visitor.visitAssignExpr(this);
+  override accept<T>(visitor: Visitor<T>): T {
+    return visitor.visitAssignExpr(this);
   }
 }
 
 class Binary extends Expr {
-  override accept(visitor: Visitor): void {
-    visitor.visitBinaryExpr(this);
+  constructor(
+    public readonly left: Expr,
+    public readonly operator: Token,
+    public readonly right: Expr
+  ) {
+    super();
+  }
+
+  override accept<T>(visitor: Visitor<T>): T {
+    return visitor.visitBinaryExpr(this);
   }
 }
 
 class Call extends Expr {
-  override accept(visitor: Visitor): void {
-    visitor.visitCallExpr(this);
+  override accept<T>(visitor: Visitor<T>): T {
+    return visitor.visitCallExpr(this);
   }
 }
 
 class Get extends Expr {
-  override accept(visitor: Visitor): void {
-    visitor.visitGetExpr(this);
+  override accept<T>(visitor: Visitor<T>): T {
+    return visitor.visitGetExpr(this);
   }
 }
 
 class Grouping extends Expr {
-  override accept(visitor: Visitor): void {
-    visitor.visitGroupingExpr(this);
+  constructor(public readonly expression: Expr) {
+    super();
+  }
+
+  override accept<T>(visitor: Visitor<T>): T {
+    return visitor.visitGroupingExpr(this);
   }
 }
 
 class Literal extends Expr {
-  override accept(visitor: Visitor): void {
-    visitor.visitLiteralExpr(this);
+  constructor(public readonly value: any) {
+    super();
+  }
+  override accept<T>(visitor: Visitor<T>): T {
+    return visitor.visitLiteralExpr(this);
   }
 }
 
 class Logical extends Expr {
-  override accept(visitor: Visitor): void {
-    visitor.visitSetExpr(this);
+  override accept<T>(visitor: Visitor<T>): T {
+    return visitor.visitSetExpr(this);
   }
 }
 
 class SetExpr extends Expr {
-  override accept(visitor: Visitor): void {
-    visitor.visitSetExpr(this);
+  override accept<T>(visitor: Visitor<T>): T {
+    return visitor.visitSetExpr(this);
   }
 }
 
 class Super extends Expr {
-  override accept(visitor: Visitor): void {
-    visitor.visitSuperExpr(this);
+  override accept<T>(visitor: Visitor<T>): T {
+    return visitor.visitSuperExpr(this);
   }
 }
 
 class This extends Expr {
-  override accept(visitor: Visitor): void {
-    visitor.visitThisExpr(this);
+  override accept<T>(visitor: Visitor<T>): T {
+    return visitor.visitThisExpr(this);
   }
 }
 
 class Unary extends Expr {
-  override accept(visitor: Visitor): void {
-    visitor.visitUnaryExpr(this);
+  constructor(public readonly operator: Token, public readonly right: Expr) {
+    super();
+  }
+  override accept<T>(visitor: Visitor<T>): T {
+    return visitor.visitUnaryExpr(this);
   }
 }
 
 class Variable extends Expr {
-  override accept(visitor: Visitor): void {
-    visitor.visitVariableExpr(this);
+  override accept<T>(visitor: Visitor<T>): T {
+    return visitor.visitVariableExpr(this);
   }
 }
+
+export {
+  Expr,
+  Visitor,
+  Assign,
+  Binary,
+  Call,
+  Get,
+  Grouping,
+  Literal,
+  Logical,
+  SetExpr,
+  Super,
+  This,
+  Unary,
+  Variable,
+};
---
+++ lib/__tests__/astPrinter.test.ts
@@ -0,0 +1,15 @@
+import { TokenType } from '../TokenType';
+import { Binary, Grouping, Literal, Unary } from '../Expr';
+import { Token } from '../Token';
+import { AstPrinter } from '../AstPrinter';
+
+it('should create binary expression', () => {
+  const expected = '(* (- 123) (group 45.67))';
+  const expression = new Binary(
+    new Unary(new Token(TokenType.MINUS, '-', null, 1), new Literal(123)),
+    new Token(TokenType.STAR, '*', null, 1),
+    new Grouping(new Literal(45.67))
+  );
+
+  expect(new AstPrinter().print(expression)).toEqual(expected);
+});
