6cfef63fd1bb3d3846f69e1096b883611847a6ef
Benjamin Petermann <benjamin.petermann@gmx.at>
2024-03-29 10:29:06 +0100

    Add lox callable to class

--- lib/LoxClass.ts
+++ lib/LoxClass.ts
@@ -1,5 +1,17 @@
-export class LoxClass {
+import { NullableObj } from './@types';
+import { Interpreter } from './Interpreter.js';
+import { LoxCallable } from './LoxCallble.js';
+import { LoxInstance } from './LoxInstance.js';
+
+export class LoxClass implements LoxCallable {
+  public readonly arity: number = 0;
+
   constructor(public readonly name: string) {}
+
+  call(interpreter: Interpreter, args: NullableObj[]): NullableObj {
+    const instance = new LoxInstance(this);
+    return instance;
+  }
 
   toString() {
     return this.name;
